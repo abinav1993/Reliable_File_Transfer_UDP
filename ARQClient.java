34519e4cfa042e2d1ff4ef867178e5bc823ae3ab
apothug <apothug@ncsu.edu>
2017-05-13 09:52:47 -0400

    Initial commit

---
+++ ARQClient.java
@@ -0,0 +1,163 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+public class ARQClient {
+	private byte[] data;
+	private InetAddress serverIP;
+	private int serverport;
+	private int n;
+	private int mss;
+	private String path;
+	private DatagramSocket client;
+	private long[] timer;
+	public ARQClient(InetAddress serverIP, int serverport, int n, int mss, String filepath){
+		this.serverIP = serverIP;
+		this.serverport = serverport;
+		this.n = n;
+		this.mss = mss;
+		this.path =filepath;
+		this.timer = new long[n];
+		
+		try{
+			this.client = new DatagramSocket(9000);
+			File f = new File(path);
+			FileInputStream fis = new FileInputStream(f);
+			data = new byte[fis.available()];
+			fis.read(data);
+//			System.out.println("data length:" + data.length);
+			fis.close();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		if (args.length != 5) {
+			System.out.println("Format: <Server-host-name> <server-port> <filename/path> <N> <MSS>");
+			return;
+		}
+		String hostname = args[0];
+		int port = Integer.parseInt(args[1]);
+//		File f = new File(args[2]);
+		String path = args[2];
+		int n = Integer.parseInt(args[3]);
+		int mss = Integer.parseInt(args[4]);
+		InetAddress IPAddress = InetAddress.getByName(hostname);
+		ARQClient sender = new ARQClient(IPAddress, port, n, mss, path);
+		System.out.println(sender.transferFile());
+	}
+	
+	long transferFile() throws IOException{
+		int lastSent = 0;
+		int NoAck = 0;
+		int maxSeqNum = (int) Math.ceil((float) data.length / mss);
+		System.out.println("Total number of packets:" + maxSeqNum);
+		
+		long start = System.currentTimeMillis();
+		
+		while (true) {
+			while (lastSent - NoAck < n && lastSent < maxSeqNum) {
+				DatagramPacket p = rdt_send(data, mss, lastSent, serverIP, serverport);
+//				System.out.println("Sending packet:"+lastSent);
+//				System.out.println();
+				client.setSoTimeout(1000);
+				client.send(p);
+//				System.out.println("sending packet "+lastSent);
+				timer[lastSent % n] = System.currentTimeMillis();
+				// withOutAck.add(p);
+				lastSent++;
+			}
+			try {
+				byte[] ackData = new byte[64];
+				DatagramPacket ack = new DatagramPacket(ackData, ackData.length);
+				client.setSoTimeout(10);
+				client.receive(ack);
+				
+				int AckedSeqNum = ByteBuffer.wrap(Arrays.copyOfRange(ack.getData(), 0, 4)).getInt();
+//				System.out.println("Ack reveived for packet:"+AckedSeqNum);
+				if (AckedSeqNum == maxSeqNum || maxSeqNum == 0)
+					break;
+				NoAck = Math.max(NoAck, AckedSeqNum);
+			} catch (SocketTimeoutException e) {
+				// for(int i = NoAck; i < lastSent; i++){
+				// client.send(withOutAck.get(i));
+				// System.out.println("Resending the packets again");
+				// }
+				long time_waiting = System.currentTimeMillis() - timer[NoAck % n];
+				if(time_waiting > 1000){
+					DatagramPacket p = rdt_send(data, mss, NoAck, serverIP, serverport);
+					client.setSoTimeout(1000);
+					client.send(p);
+					System.out.println("sending packet "+NoAck);
+					timer[NoAck % n] = System.currentTimeMillis();
+				}else{
+					//System.out.println("waiting");
+				}
+				System.out.println("Timeout, sequence number = " + NoAck);
+			}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Sending End of File packet");
+		byte[] segment = createSegment("EOF".getBytes(), lastSent, "EOF");
+		DatagramPacket p = new DatagramPacket(segment, segment.length, serverIP, serverport);
+		client.send(p);
+		client.close();
+		return end - start;
+	}
+	
+	DatagramPacket rdt_send(byte[] data, int mss, int lastSent, InetAddress IPAddress, int port){
+		byte[] pData = new byte[mss];
+		pData = Arrays.copyOfRange(data, lastSent * mss,
+				((lastSent * mss) + mss) > data.length ? data.length : (lastSent * mss) + mss);
+//		for(int i=0;i<Math.min(pData.length,15);i++){
+//			System.out.print(pData[i] + " ");
+//		}
+		byte[] segment = createSegment(pData, lastSent, "data");
+		DatagramPacket p = new DatagramPacket(segment, segment.length, IPAddress, port);
+		return p;
+	}
+	
+	long checksum(byte [] b)
+    {  
+		int i = 0, length = b.length;
+		long sum = 0;
+		while (length > 0) {
+			sum += (b[i++] & 0xff) << 8;
+			if ((--length) == 0)
+				break;
+			sum += (b[i++] & 0xff);
+			--length;
+		}
+		return ((~((sum & 0xFFFF) + (sum >> 16))) & 0xFFFF);
+    }
+
+	byte[] createSegment(byte[] data, int seq, String type){
+		byte[] segment = new byte[8];
+		ByteBuffer b = ByteBuffer.allocate(4);
+		b.putInt(seq);
+		byte[] sequence = b.array();//getSequenceNumber(seq);
+		b = ByteBuffer.allocate(2);
+		if(type.equals("data"))
+			b.putShort((short)21845);  // Packet type 16 bit: 0101010101010101
+		else
+			b.putShort((short)0);
+		byte[] dataPacket = b.array(); 
+		
+		short checksum = (short)checksum(data);
+		//b = ByteBuffer.allocate(2);
+		//b.putShort((short)checksum);  
+		byte[] check = new byte[] { (byte) (checksum >>> 8), (byte) checksum };
+		//System.out.println("Bytes of Checksum:" + check[0] + " " + check[1]);
+
+		System.arraycopy(sequence, 0, segment, 0, sequence.length);
+		System.arraycopy(check, 0, segment, 4, check.length);
+		System.arraycopy(dataPacket, 0, segment, 6, dataPacket.length);
+		byte result[] = new byte[segment.length + data.length];
+		System.arraycopy(segment, 0, result, 0, segment.length);
+		
+		System.arraycopy(data, 0, result, 8, data.length);
+		return result;	
+	}
+}
---
+++ ARQServer.java
@@ -0,0 +1,149 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
+public class ARQServer {
+
+	private int port;
+	private String fname;
+	private float p;
+	private Random r;
+	private DatagramSocket server;
+	private int window;
+	private byte buffer[][];
+	public ARQServer(int port, String fname, float p, int window){
+		this.port = port;
+		this.fname = fname;
+		this.p = p;	
+		this.window = window;
+		this.r = new Random();
+		this.buffer = new byte[window][];
+		try {
+			this.server = new DatagramSocket(port);
+		} catch (SocketException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	void receiveFile() throws Exception{
+		File f = new File(fname);
+		FileOutputStream fos = new FileOutputStream(f);
+		byte[] receivedSegment;
+		int waitingFor = 0;
+
+		while (true) {
+			receivedSegment = new byte[4096];
+			DatagramPacket receivedPacket = new DatagramPacket(receivedSegment, receivedSegment.length);
+			server.receive(receivedPacket);
+			int seqNum = ByteBuffer.wrap(Arrays.copyOfRange(receivedPacket.getData(), 0, 4)).getInt();
+			short sender_checksum = ByteBuffer.wrap(Arrays.copyOfRange(receivedPacket.getData(), 4, 6)).getShort();
+//			byte byte0 = receivedSegment[4]; byte byte1 = receivedSegment[5];
+//			System.out.println("Bytes of Checksum:" + byte0 + " " + byte1);
+//			int sender_checksum_test = (short) ((byte1 & 0xFF) + ((byte0 & 0xFF) << 8));
+//			System.out.println("Sender Checksum test:"+ sender_checksum_test);
+//			for(int i=0; i<8;i++){
+//				System.out.print(receivedSegment[i] + " ");
+//			}
+//			System.out.println();
+			
+			short packetType = ByteBuffer.wrap(Arrays.copyOfRange(receivedPacket.getData(), 6, 8)).getShort();
+			byte[] data = new byte[receivedPacket.getLength() - 8];
+			System.arraycopy(receivedSegment, 8, data, 0, data.length);
+			
+			if(packetType == 0){
+				System.out.println("Reached End of File");
+				fos.flush();
+				fos.close();
+				server.close();
+				break;
+			}
+			
+			float randomNumber = r.nextFloat();
+//			System.out.println("RandomNumber: "+randomNumber);
+			if (randomNumber <= p) {
+				System.out.println("Packet loss, sequence number = " + seqNum);
+			} else {
+				InetAddress from = receivedPacket.getAddress();
+				int portFrom = receivedPacket.getPort();
+				short checkSumVerify = (short) checksum(data);
+//				System.out.println("Got:"+seqNum + " Looking for:"+waitingFor);
+//				System.out.println("Original Checksum:"+sender_checksum + " & Calculated Checksum:"+ checkSumVerify);
+//				for(int i=0;i<Math.min(data.length, 15);i++){
+//					System.out.print(data[i] + " ");
+//				}
+//				System.out.println();
+				if (checkSumVerify == sender_checksum) {
+					// System.out.println("Checksum verified");
+						if(buffer[seqNum % window] == null){
+							buffer[seqNum % window] = data;
+						}
+						while(buffer[waitingFor % window] != null){
+							fos.write(buffer[waitingFor % window]);
+							buffer[waitingFor % window] = null;
+							waitingFor++;
+						}
+	
+						byte[] segment = createAckSegment(waitingFor);
+						DatagramPacket ack = new DatagramPacket(segment, segment.length, from, portFrom);
+//						System.out.println("Sending Ack for seqNum:"+waitingFor);
+						server.send(ack);
+					
+				}
+			}
+		}
+		fos.flush();
+		fos.close();
+		server.close();
+	}
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		if (args.length != 4) {
+			System.out.println("Format: <Server port> <Filename> <Probability> <window-size>");
+			return;
+		}
+		int port = Integer.parseInt(args[0]);
+		String filename = args[1];
+		float p = Float.parseFloat(args[2]);
+		int window = Integer.parseInt(args[3]);
+		
+		ARQServer receiver = new ARQServer(port, filename, p, window);
+		receiver.receiveFile();
+	}
+
+	
+	byte[] createAckSegment(int seqNum) {
+		byte[] segment = new byte[8];
+		int ackPacket = 43690; // Packet type 16 bit: 1010101010101010
+		int other = 0;
+		ByteBuffer b = ByteBuffer.allocate(4);
+		b.putInt(seqNum);
+		byte[] sequence = b.array();
+		b = ByteBuffer.allocate(2);
+		b.putShort((short)ackPacket);
+		byte[] dataPacket = b.array();
+		b = ByteBuffer.allocate(2);
+		b.putShort((short)other);
+		byte[] empty = b.array();
+		
+		System.arraycopy(sequence, 0, segment, 0, sequence.length);
+		System.arraycopy(empty, 0, segment, 4, empty.length);
+		System.arraycopy(dataPacket, 0, segment, 6, dataPacket.length);
+		
+		return segment;
+	}
+
+	long checksum(byte [] b)
+    {  
+		int i = 0, length = b.length;
+		long sum = 0;
+		while (length > 0) {
+			sum += (b[i++] & 0xff) << 8;
+			if ((--length) == 0)
+				break;
+			sum += (b[i++] & 0xff);
+			--length;
+		}
+		return ((~((sum & 0xFFFF) + (sum >> 16))) & 0xFFFF);
+    }
+}
